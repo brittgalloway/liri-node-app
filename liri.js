6e3232f1317fc39142b75bc13ec166b0e5bd97bb
BrittGall <crlnfllr@gmail.com>
2019-10-05 12:28:38 -0500

    spotify works

--- liri.js
+++ liri.js
@@ -6,24 +6,22 @@ const Spotify = require("node-spotify-api");
 const spotify = new Spotify(keys.spotifyKeys);
 
 //SPOTIFY=============================================================
-//   Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
+
 if (process.argv[2] == "spotify-this-song") {
-  spotify.search({ type: "track", query: process.argv[3], limit: 5 }, function(
+  spotify.search({ type: "track", query: process.argv[3] }, function(
     err,
     data
   ) {
     if (err) {
       return console.log("Error occurred: " + err);
     }
+    console.log("Song: " + data.tracks.items[0].name);
+    console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
 
-    console.log("Song Name: " + data.tracks.items[0].album.name);
     console.log(
       "Preview Song: " + data.tracks.items[0].album.external_urls.spotify
     );
-    console.log(data.tracks.items[0].album);
+    console.log("Album Name: " + data.tracks.items[0].album.name);
   });
 }
 // MOVIE API ==========================================================
